91ca9e86124ceb16327fcce51ad18f48b27f8cc1
rslbn <ronyslbn7@gmail.com>
2025-05-15 22:38:52 +0700

    add new method query

--- src/main/kotlin/com/fastcampus/blog/repository/UserRepository.kt
+++ src/main/kotlin/com/fastcampus/blog/repository/UserRepository.kt
@@ -1,8 +1,11 @@
 package com.fastcampus.blog.repository
 
-import com.fastcampus.blog.entity.User
+import com.fastcampus.blog.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository: JpaRepository<User, Long> {
 
+   fun findByUsernameAndIsDeleted(username: String, isDeleted: Boolean): User?
+   fun existsByUsernameAndIsDeleted(username: String, isDeleted: Boolean): Boolean
+   fun existsByEmail(email: String): Boolean
 }
