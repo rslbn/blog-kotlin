6f48f7491874bdcb98c968198f1350866a22a18e
rslbn <ronyslbn7@gmail.com>
2025-05-28 08:18:11 +0700

    update

--- src/main/resources/db/migration/V6__insert_roles_data.sql
+++ src/main/resources/db/migration/V6__insert_roles_data.sql
@@ -1,3 +1,4 @@
+INSERT INTO ROLES (name, description) VALUES ('SUPERADMIN', 'Super admin role');
 INSERT INTO ROLES (name, description) VALUES ('ADMIN', 'Admin role');
 INSERT INTO ROLES (name, description) VALUES ('AUTHOR', 'Write, view, update, delete articles and comment on articles');
 INSERT INTO ROLES (name, description) VALUES ('STANDARD', 'View articles, and comment on articles');
